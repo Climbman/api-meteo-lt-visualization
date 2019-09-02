9c49352d2710693975b80e7d4dba657b25e841ba
Laurynas <laurynas.kr@hotmail.com>
2019-09-02 20:57:10 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function get(url, callback) {
+    var http = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            callback(this);
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
