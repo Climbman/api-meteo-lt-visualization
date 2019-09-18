f73920acb724ee4551839720395d5e0f07bbd5e0
Laurynas <laurynas.kr@hotmail.com>
2019-09-18 20:16:28 +0300

    some work on data structure transformation

--- main.js
+++ main.js
@@ -1,38 +1,76 @@
+//globals for storing graph point data
+let dates;
+let temperature;
+let wind_speed;
+let wind_gust;
+let wind_direction;
+let cloud_cover;
+let sea_pressure;
+let precipitation;
+let condition_code;
+
+
+
+//adding event listeners
 window.onload = function() {
-    //adding event listeners
     document.getElementById('fld_search').addEventListener('keyup', suggest);
     document.getElementById('fld_search').addEventListener('change', unfocus_input);
-    document.getElementById('btn_confirm').addEventListener('click', refresh_graph)
+    document.getElementById('btn_confirm').addEventListener('click', refresh_graph);
     
     //runing js to load data
     
 };
 
+
+//functions for retrieving graph data
 function refresh_graph() {
-    var place = document.getElementById('fld_search').value;
-    get('srv.php?cmd=2&place=' + place, function() {
-        console.log(this.responseText);
-    });
+    let place = document.getElementById('fld_search').value;
+    get('srv.php?cmd=2&place=' + place, set_graph_data);
 }
 
-function unfocus_input(event) {
-    event.target.blur();
+function set_graph_data(response) {
+    //unset previous values
+    dates = null;
+    temperature = null;
+    wind_speed = null;
+    wind_gust = null;
+    wind_direction = null;
+    cloud_cover = null;
+    sea_pressure = null;
+    precipitation = null;
+    condition_code = null;
+    
+    //to array
+    let data_points = JSON.parse(response.responseText);
+    
+    if (!data_points) {
+        return false;
+    }
+    
+    for (let i = 0; i < data_points.length; i++) {
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+    }
 }
 
+
+
+//functions for place suggestions
 function suggest(event) {
     get('srv.php?cmd=1&search_phrase=' + event.target.value, set_places);
 }
 
-function set_graph_data() {
-}
-
-
 function set_places(response_obj) {
-    var response;
-    var places;
-    var option;
+    let response;
+    let places;
+    let option;
     
-    var container = document.getElementById('places');
+    let container = document.getElementById('places');
     
     response = JSON.parse(response_obj.responseText);
     
@@ -47,17 +85,18 @@ function set_places(response_obj) {
     
     places = response.matches;
     
-    
     container.innerHTML = '';
-    for (var i = 0; i < places.length; i++) {
+    for (let i = 0; i < places.length; i++) {
         option = document.createElement('option');
         option.setAttribute('value', places[i]);
         container.appendChild(option);
     }
 }
 
+
+//General functions
 function get(url, callback) {
-    var xhttp = new XMLHttpRequest();
+    let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             callback(this);
@@ -65,4 +104,9 @@ function get(url, callback) {
     };
     xhttp.open("GET", url, true);
     xhttp.send();
+}
+
+function unfocus_input(event) {
+    console.log('unfocus');
+    event.target.blur();
 }
